a37ebbe2093a7c9f86183c3ba57f78fe78fd9668
PEDRINHOMAISLINDO <Pedro.GitHub7@gmail.com>
2025-03-12 18:53:17 -0300

    TOCANDO UM MP3
    Faça um programa em Python que abra e reproduza o áudio de um arquivo MP3.

---
+++ EXERCÍCIO_21.py
@@ -0,0 +1,6 @@
+from playsound3 import playsound # IMPORTANDO SOMENTE A FUNÇÃO PLAYSOUND
+print('xx' * 25)
+print('EXERCÍCIO 21!')
+print('xx' * 25)
+
+playsound('dilatacao-hipnotica-6-0-brazilian-phonk.mp3')#FUNÇÃO PARA FAZER TOCAR UM MP3
